ba8251816c4e657923f95a254326181e8c4232b8
p <pawel.gitara@onet.eu>
2018-12-19 12:26:50 +0100

    hints updated

--- app.ts
+++ app.ts
@@ -1,19 +1,1 @@
-class Greeter {
-    greeting: string;
-    constructor(message: string) {
-        this.greeting = message;
-    }
-    greet() {
-        return "Hello, " + this.greeting;
-    }
-}
 
-let greeter1 = new Greeter("worldee");
-
-let button = document.createElement('button');
-button.textContent = "Say Hello";
-button.onclick = function() {
-    alert(greeter1.greet());
-}
-
-document.body.appendChild(button);
--- hints.js
+++ hints.js
@@ -1,2 +1,15 @@
 // tsc --init : puts this folder under control of npm and under control of tyescript, after running this command
 // json file will be created, and then we can just type tsc to compile all ts files in our folder to js files
+
+// 1. JS - dynamic types
+//    TS - static types, we define it once and then we cant change the type variable stores.
+//    eg. let data = 'Peter" 
+//        let data = 21 > error, you cant change type 
+//          
+//    number : 21, 55.5
+
+//    if we just declare variable without initialization it will have type of 'any' 
+//      so then we can reasign event with other types eg:
+//      let data 
+//      data = 'qwerty'
+//      data = 12 > ok, no errors
